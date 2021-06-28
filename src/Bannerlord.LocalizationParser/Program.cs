223b4e9870e9f3aead99acc535c33f0c44685e68
aragas <va_mikhaylov@head-point.ru>
2021-06-28 12:50:22 +0300

    Init

--- src/Bannerlord.LocalizationParser/Program.cs
+++ src/Bannerlord.LocalizationParser/Program.cs
@@ -1,4 +1,6 @@
-﻿using CsvHelper;
+﻿using CommandLine;
+
+using CsvHelper;
 using CsvHelper.Configuration;
 
 using ICSharpCode.Decompiler;
@@ -16,6 +18,15 @@ using System.Reflection.PortableExecutable;
 
 namespace Bannerlord.LocalizationParser
 {
+    public class Options
+    {
+        [Option('g', "gameFolder", Required = true, HelpText = "Set output to verbose messages.")]
+        public string? GameFolder { get; set; }
+
+        [Option('o', "output", Required = true, HelpText = "Set output to verbose messages.")]
+        public string? Output { get; set; }
+    }
+
     public static class Program
     {
         private record LocalizationString(string Assembly, string Text) { }
@@ -103,6 +114,8 @@ namespace Bannerlord.LocalizationParser
 
         private static void ParseModule(DirectoryInfo modulesFolder, string module)
         {
+            Console.WriteLine($"Checking module {module}...");
+
             var moduleFolder = modulesFolder.GetDirectories(module)
                 .FirstOrDefault()?
                 .GetDirectories("bin")
@@ -118,49 +131,88 @@ namespace Bannerlord.LocalizationParser
             }
         }
 
-        public static void Main(string gameFolder, string output)
+        public static void Main(string[] args) => Parser.Default.ParseArguments<Options>(args)
+            .WithNotParsed(o =>
+            {
+                Console.WriteLine("Received incorrect input. Using interactive mode!");
+                Console.WriteLine("Please write the directory location path to look for strings!");
+                var gameFolder = Console.ReadLine();
+                Console.WriteLine("Please write the output directory path!");
+                var output = Console.ReadLine();
+
+                Execute(gameFolder, output);
+            })
+            .WithParsed(o =>
+            {
+                Execute(o.GameFolder, o.Output);
+            });
+
+        public static void Execute(string? gameFolder, string? output)
         {
             var directoryInfo = new DirectoryInfo(gameFolder);
 
             if (!directoryInfo.Exists)
             {
-                Console.WriteLine("A non existing folder was provided!");
+                Console.WriteLine("A non existing directory location for was provided!");
                 return;
             }
 
             var folders = directoryInfo.GetDirectories();
+
+            var noBinFolder = false;
             if (folders.All(di => di.Name != "bin"))
             {
-                Console.WriteLine("The directory provided does not have 'bin' folder!");
-                return;
+                noBinFolder = true;
+                Console.WriteLine("The directory provided does not have 'bin' directory!");
+                //return;
             }
+            else
+            {
+                Console.WriteLine("Checking 'bin/Win64_Shipping_Client'...");
+
+                var mainFolder = directoryInfo.GetDirectories("bin")
+                    .FirstOrDefault()?
+                    .GetDirectories("Win64_Shipping_Client")
+                    .FirstOrDefault();
+                if (mainFolder is null || !mainFolder.Exists)
+                {
+                    Console.WriteLine("The directory provided does not have 'bin/Win64_Shipping_Client' folder!");
+                    return;
+                }
+                foreach (var fileInfo in mainFolder.GetFiles("*.dll"))
+                {
+                    ParseLibrary(fileInfo);
+                }
+            }
+
+            var noModules = false;
             if (folders.All(di => di.Name != "Modules"))
             {
-                Console.WriteLine("The directory provided does not have 'Modules' folder!");
-                return;
+                noModules = true;
+                Console.WriteLine("The directory provided does not have 'Modules' directory!");
+                //return;
             }
-
-            var mainFolder = directoryInfo.GetDirectories("bin")
-                .FirstOrDefault()?
-                .GetDirectories("Win64_Shipping_Client")
-                .FirstOrDefault();
-            if (mainFolder is null || !mainFolder.Exists)
+            else
             {
-                Console.WriteLine("The directory provided does not have 'bin/Win64_Shipping_Client' folder!");
-                return;
+                Console.WriteLine("Checking modules...");
+
+                var modulesFolder = directoryInfo.GetDirectories("Modules").First();
+                ParseModule(modulesFolder, "Native");
+                ParseModule(modulesFolder, "SandBox");
+                ParseModule(modulesFolder, "SandBoxCore");
+                ParseModule(modulesFolder, "StoryMode");
+                ParseModule(modulesFolder, "CustomBattle");
             }
-            foreach (var fileInfo in mainFolder.GetFiles("TaleWorlds.*.dll"))
+
+            if (noBinFolder && noModules)
             {
-                ParseLibrary(fileInfo);
+                Console.WriteLine("The provided directory is not a valid M&B:2 directory! Checking all available .dll's within the directory...");
+
+                foreach (var fileInfo in directoryInfo.GetFiles("*.dll"))
+                {
+                    ParseLibrary(fileInfo);
+                }
             }
-
-            var modulesFolder = directoryInfo.GetDirectories("Modules").First();
-            ParseModule(modulesFolder, "Native");
-            ParseModule(modulesFolder, "SandBox");
-            ParseModule(modulesFolder, "SandBoxCore");
-            ParseModule(modulesFolder, "StoryMode");
-            ParseModule(modulesFolder, "CustomBattle");
-
 
             using var writer = new StreamWriter(output);
             using var csv = new CsvWriter(writer, new CsvConfiguration(CultureInfo.InvariantCulture)
